052998e7c303cc998a8142a0d892e3b3f1af1064
Christopher Schnick <st141923@stud.uni-stuttgart.de>
2023-01-05 23:36:53 +0100

    Apply several fixes to wsl script

--- install_wsl.sh
+++ install_wsl.sh
@@ -1,31 +1,43 @@
-docker
+#!/usr/bin/env bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Invalid number of arguments. Usage: install_wsl.sh <Windows EU4 installation directory> <Installation EU4 version>"
+    exit
+fi
+
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Not root! Run as root"
+    exit
+fi
+
+docker > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
 	echo "Please set up Docker Desktop on windows and install the WSL integration as well such that the docker command can be called from WSL"
 	exit
 fi
 
+set -e
 apt-get update
 
+# Install misc packages
+apt-get install -y gcc g++ make imagemagick unzip libssl-dev pkg-config curl
+
 # Install node js
-curl -fsSL https://deb.nodesource.com/setup_19.x | bash -i
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash - &&\
 apt-get install -y nodejs
 
 # Install rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -i
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
 
 # Install just
-curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
-echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | tee /etc/apt/sources.list.d/prebuilt-mpr.list
-apt-get install -y just
-
-# Install other packages
-apt-get install -y imagemagick unzip libssl-dev pkg-config
+cargo install just
 
 # Set up
 just setup
 
 # Install EU4 bundle
-EU4_DIR=wslpath "$1"
+EU4_DIR=$(wslpath "$1")
 VERSION="$2"
 just pdx create-bundle "$EU4_DIR" assets/game-bundles
 just pdx compile-assets "assets/game-bundles/eu4-${VERSION}.tar.zst"
